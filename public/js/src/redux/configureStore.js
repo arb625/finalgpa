088977f2d2a7696a9159e5d19436f32c5d7ccf1c
Anurag Baddam <baddamanu@berkeley.edu>
2016-09-10 03:14:01 -0700

    no redux tools

--- public/js/src/redux/configureStore.js
+++ public/js/src/redux/configureStore.js
@@ -4,7 +4,8 @@ import {persistStore, autoRehydrate} from 'redux-persist';
 
 export default function configureStore (initialState = {}) {
 	// const store = createStore(rootReducer, initialState, window.devToolsExtension && window.devToolsExtension());
-  const store = createStore(rootReducer, initialState, compose(autoRehydrate(), window.devToolsExtension && window.devToolsExtension()));
+  // const store = createStore(rootReducer, initialState, compose(autoRehydrate(), window.devToolsExtension && window.devToolsExtension()));
+  const store = createStore(rootReducer, initialState, autoRehydrate());
   persistStore(store);
   return store;
 }
