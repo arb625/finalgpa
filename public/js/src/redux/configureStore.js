a0e15cd0c0ecb78a40435d9f272c2a5ac00838a2
Anurag Baddam <baddamanu@berkeley.edu>
2016-09-11 20:24:01 -0700

    test comments

--- public/js/src/redux/configureStore.js
+++ public/js/src/redux/configureStore.js
@@ -3,8 +3,12 @@ import rootReducer from './rootReducer';
 import {persistStore, autoRehydrate} from 'redux-persist';
 
 export default function configureStore (initialState = {}) {
+	// test comment 1
 	// const store = createStore(rootReducer, initialState, window.devToolsExtension && window.devToolsExtension());
   // const store = createStore(rootReducer, initialState, compose(autoRehydrate(), window.devToolsExtension && window.devToolsExtension()));
+  
+  // test comment 2
+
   const store = createStore(rootReducer, initialState, autoRehydrate());
   persistStore(store);
   return store;
